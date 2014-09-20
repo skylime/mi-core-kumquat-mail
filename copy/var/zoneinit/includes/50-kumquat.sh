cb17472108305d3ec3e414072d414c36b992ffed
Thomas Merkel <tm@core.io>
2014-09-20 15:40:09 +0200

    extra folder for sqlite db

--- copy/var/zoneinit/includes/50-kumquat.sh
+++ copy/var/zoneinit/includes/50-kumquat.sh
@@ -24,7 +24,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '${ALLOWED_HOST}']
 DATABASES = {
         'default': {
                 'ENGINE':   'django.db.backends.sqlite3',
-                'NAME':     os.path.join(BASE_DIR, 'db.sqlite3'),
+                'NAME':     '/var/sqlite/kumquat.sqlite3',
         },
 }
 
