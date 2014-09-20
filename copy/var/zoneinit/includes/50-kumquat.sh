965effdb598741433c656d7e923223d102d6bcdd
Thomas Merkel <tm@core.io>
2014-09-20 15:34:13 +0200

    be sure to create a valid user

--- copy/var/zoneinit/includes/50-kumquat.sh
+++ copy/var/zoneinit/includes/50-kumquat.sh
@@ -36,7 +36,5 @@ EOF
 /opt/kumquat/manage.py syncdb --noinput
 
 # Create superadmin user
-if [[ ${DB_CREATED} == true ]]; then
-	echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', '${ADMIN_KUMQUAT}')" \
-		| /opt/kumquat/manage.py shell
-fi
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', '${ADMIN_KUMQUAT}')" \
+	| /opt/kumquat/manage.py shell
