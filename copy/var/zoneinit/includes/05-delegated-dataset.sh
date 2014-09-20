3ffe621f4af25047dbf45fbffb1adb83b7849a7e
Thomas Merkel <tm@core.io>
2014-09-20 15:39:12 +0200

    delegate dataset for sqlite db

---
+++ copy/var/zoneinit/includes/05-delegated-dataset.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+UUID=$(mdata-get sdc:uuid)
+DDS=zones/${UUID}/data
+
+if zfs list ${DDS} 1>/dev/null 2>&1; then
+	zfs create ${DDS}/sqlite || true
+
+	zfs set mountpoint=/var/sqlite ${DDS}/sqlite
+fi
